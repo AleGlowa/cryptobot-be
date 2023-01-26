968fcf5317122ec4ddb3be7698911348aa8bf210
Alex Glowacki <klopsmonster@gmail.com>
2023-01-26 16:09:52 +0100

    remove delay

--- api/src/main/scala/cryptobot/exchange/bybit/Boot.scala
+++ api/src/main/scala/cryptobot/exchange/bybit/Boot.scala
@@ -23,8 +23,8 @@ object Boot extends ZIOAppDefault:
     ZIO.scoped(
       for
         _ <- Console.printLine(s"Starting the server at http://localhost:8080").orDie
-        _ <- Server.start(8080, app).forkScoped
-        _ <- Console.readLine("Press ENTER to stop the application\n").delay(1.second).orDie
+        _ <- Server.start(8080, app)
+        _ <- Console.readLine("Press ENTER to stop the application\n").orDie
         _ <- Console.printLine("Stopping the application...").orDie
       yield ()
     )
