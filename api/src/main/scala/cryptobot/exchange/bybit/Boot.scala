9cff60c089656fe658ed6410b55b0c70cbeddae7
Alex Glowacki <klopsmonster@gmail.com>
2023-03-08 09:55:58 +0100

    cors for http and ws

--- api/src/main/scala/cryptobot/exchange/bybit/Boot.scala
+++ api/src/main/scala/cryptobot/exchange/bybit/Boot.scala
@@ -5,19 +5,34 @@ import zhttp.http.*
 import zhttp.socket.*
 import zhttp.service.*
 import zhttp.service.ChannelEvent.*
+import zhttp.http.middleware.Cors.CorsConfig
 
 import cryptobot.config.Config
 import cryptobot.exchange.bybit.ws.{ WsApp, InverseWsApp }
 
 object Boot extends ZIOAppDefault:
 
-  private val inverseWsApp = new InverseWsApp
+  private val feOrigin = "http://localhost:3000"
 
+  private val corsConfig =
+    CorsConfig(
+      anyOrigin = false,
+      anyMethod = false,
+      allowedOrigins = _ == feOrigin,
+      allowedMethods = Some(Set(Method.GET))
+    )
+
+  private val inverseWsApp = new InverseWsApp
   private val app =
     Http.collectZIO[Request] {
-      case Method.GET -> !! / "inverseWs" / "subscriptions" =>
-        inverseWsApp.msgOutLogic.toResponse
-    }
+      case req @ Method.GET -> !! / "inverseWs" / "subscriptions" =>
+        val isFeOrigin = req.origin.exists(feOrigin contentEquals _)
+
+        if isFeOrigin then
+          inverseWsApp.msgOutLogic.toResponse
+        else
+          ZIO.succeed(Response.status(Status.Forbidden))
+    } @@ Middleware.cors(corsConfig)
 
   override val run: UIO[ExitCode] =
     ZIO.scoped(
